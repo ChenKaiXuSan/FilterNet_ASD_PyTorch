c70a078926ff244e31a55e5d213e80cc5f106818
Xu <chenkaixusan@gmail.com>
2025-07-11 02:07:43 +0000

    修复路径拼接错误并确保检查点目录存在

--- filter/main.py
+++ filter/main.py
@@ -135,7 +135,7 @@ def train(hparams: DictConfig, dataset_idx, fold: int):
      
     shutil.copyfile(best_model_path, log_best_model_path)
     # copy for the filter score inference.
-    _save_path = os.path.join("/ckpt", f"{hparams.train.phase}/{str(fold)}_best_model.ckpt"
+    _save_path = os.path.join("/ckpt", f"{hparams.train.phase}/{str(fold)}_best_model.ckpt")
     if os.path.exists("/ckpt") is False:
         os.makedirs("/ckpt")
 
